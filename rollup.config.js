1a6bbfe0dc6932247ab382e135f08ff6665b55b5
郑鹏飞 <zhengpengfei@elephx.io>
2023-08-09 22:28:31 +0800

    fix: ts 配置去掉 outDir 等

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
 import typescript from '@rollup/plugin-typescript'
-import terser  from '@rollup/plugin-terser'
+import terser from '@rollup/plugin-terser'
 
 import dts from 'rollup-plugin-dts'
 
@@ -31,19 +31,19 @@ export default () => {
     config.push(template(mouldeName))
   })
 
-  if(process.env.NODE_ENV === 'production'){
-    config.forEach(val=>{
+  if (process.env.NODE_ENV === 'production') {
+    config.forEach(val => {
       val.plugins.push(
         terser({
-          mangle:false
+          mangle: false
         }))
     })
   }
 
   config.push({
-    input: "./src/index.ts",
-    output: [{ file: "lib/types/index.d.ts", format: "es" }],
-    plugins: [dts()],
+    input: './src/index.ts',
+    output: [{ file: 'lib/types/index.d.ts', format: 'es' }],
+    plugins: [dts()]
   })
 
   return config
